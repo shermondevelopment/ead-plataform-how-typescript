d6ae5b9f1ed05a1162ee61c236fabe9421c5fbf6
Victor Shermon <victorshermon@yahoo.com>
2021-05-10 22:31:07 -0300

    chore: add typeorm in project

---
+++ src/infra/db/mongodb/migration/1620692913517-CreateAccountsTable.ts
@@ -0,0 +1,46 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm'
+
+export class CreateAccountsTable1620692913517 implements MigrationInterface {
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name: 'accounts',
+                columns: [
+                    {
+                        name: 'id',
+                        type: 'uuid',
+                        isPrimary: true,
+                        generationStrategy: 'uuid'
+                    },
+                    {
+                        name: 'name',
+                        type: 'varchar',
+                        isNullable: false
+                    },
+                    {
+                        name: 'email',
+                        type: 'varchar',
+                        isNullable: false,
+                        isUnique: true
+                    },
+                    {
+                        name: 'password',
+                        type: 'varchar',
+                        isNullable: false
+                    },
+                    {
+                        name: 'sexo',
+                        type: 'enum',
+                        enum: ['M', 'F'],
+                        isNullable: false
+                    }
+                ]
+            }),
+            true
+        )
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable('accounts')
+    }
+}
