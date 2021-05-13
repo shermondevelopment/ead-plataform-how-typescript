da7ff843a95931ca0a1e7f29094940605f655855
Victor Shermon <victorshermon@yahoo.com>
2021-05-13 20:58:43 -0300

    refactor: connect to mongodb before integration tests

--- src/main/routes/signup-routes.test.ts
+++ src/main/routes/signup-routes.test.ts
@@ -1,7 +1,22 @@
 import request from 'supertest'
 import app from '../config/app'
+import { MysqlHelper } from '../../infra/db/mysql/helpers/mysql-helper'
+import Accounts from '../../infra/db/mysql/entity/accounts'
 
 describe('Signup Routes', () => {
+    beforeAll(async () => {
+        await MysqlHelper.connect()
+    })
+
+    afterAll(async () => {
+        await MysqlHelper.disconnect()
+    })
+
+    beforeEach(async () => {
+        const accountCollection = MysqlHelper.getRepository(Accounts)
+        await accountCollection.delete({ name: 'any_name' })
+    })
+
     test('Should return an account on success', async () => {
         await request(app)
             .post('/api/signup')
