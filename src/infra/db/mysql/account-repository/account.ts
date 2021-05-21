8a867fd79c66d7c5c0cf86f88f0b9404b91c1bd5
Victor Shermon <victorshermon@yahoo.com>
2021-05-21 20:39:52 -0300

    feat: ensure AccountMysqlRepository returns an account on loadByEmail success

--- src/infra/db/mysql/account-repository/account.ts
+++ src/infra/db/mysql/account-repository/account.ts
@@ -5,12 +5,25 @@ import {
 } from '../../../../presentation/controllers/signup/signup-protocols'
 import { MysqlHelper } from '../helpers/mysql-helper'
 import Accounts from '../entity/accounts'
+import { LoadAccountByEmailRepository } from '../../../../data/protocols/db/load-account-by-email-repository'
+import { Repository } from 'typeorm'
 
-export class AccountMongoRepository implements AddAccountRepository {
+export class AccountMongoRepository
+    implements AddAccountRepository, LoadAccountByEmailRepository {
+    private accountRepository: Repository<Accounts>
+
+    constructor() {
+        this.accountRepository = MysqlHelper.getRepository(Accounts)
+    }
+
     async add(accountData: AddAccountModel): Promise<AccountModel> {
-        const accountRepository = MysqlHelper.getRepository(Accounts)
-        const account = accountRepository.create(accountData)
-        await accountRepository.save(account)
+        const account = this.accountRepository.create(accountData)
+        await this.accountRepository.save(account)
         return account
+    }
+
+    async loadByEmail(email: string): Promise<AccountModel> {
+        const loadByEmail = await this.accountRepository.findOne({ email })
+        return loadByEmail
     }
 }
