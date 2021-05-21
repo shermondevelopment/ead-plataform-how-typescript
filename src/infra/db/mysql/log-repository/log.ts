250b72e11f91ee13c753f14ae57e874a2b074361
Victor Shermon <victorshermon@yahoo.com>
2021-05-20 22:18:22 -0300

    refactor: move data layer interfaces to folders

--- src/infra/db/mysql/log-repository/log.ts
+++ src/infra/db/mysql/log-repository/log.ts
@@ -1,4 +1,4 @@
-import { LogErrorRepository } from '../../../../data/protocols/log-error-repository'
+import { LogErrorRepository } from '../../../../data/protocols/db/log-error-repository'
 import Errors from '../entity/errors'
 import { MysqlHelper } from '../helpers/mysql-helper'
 
