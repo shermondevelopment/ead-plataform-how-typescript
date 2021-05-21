a81048f04762663da5f0db481a966eaaac7d0241
Victor Shermon <victorshermon@yahoo.com>
2021-05-21 11:00:49 -0300

    refactor: move DbAuthentication protocols an single file

--- src/data/usecases/authentication/db-authentication.ts
+++ src/data/usecases/authentication/db-authentication.ts
@@ -1,11 +1,11 @@
 import {
+    AuthenticationModel,
+    HashCompare,
+    TokenGenerator,
+    LoadAccountByEmailRepository,
     Authentication,
-    AuthenticationModel
-} from '../../../domain/usecases/authentication/authentication'
-import { HashCompare } from '../../protocols/criptography/hash-compare'
-import { TokenGenerator } from '../../protocols/criptography/token-generator'
-import { LoadAccountByEmailRepository } from '../../protocols/db/load-account-by-email-repository'
-import { UpdateAccessTokenRepository } from '../../protocols/db/update-access-token-repository'
+    UpdateAccessTokenRepository
+} from './db-authentication-protocols'
 
 export class DbAuthentication implements Authentication {
     private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository
