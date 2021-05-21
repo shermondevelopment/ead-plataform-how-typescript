5640d2727e414d869bcfccce3e68859a2192109d
Victor Shermon <victorshermon@yahoo.com>
2021-05-21 09:02:30 -0300

    feat: ensure DbAuthentication calls UpdateAccessTokenRepository with correct values

--- src/data/usecases/authentication/db-authentication.ts
+++ src/data/usecases/authentication/db-authentication.ts
@@ -5,20 +5,24 @@ import {
 import { HashCompare } from '../../protocols/criptography/hash-compare'
 import { TokenGenerator } from '../../protocols/criptography/token-generator'
 import { LoadAccountByEmailRepository } from '../../protocols/db/load-account-by-email-repository'
+import { UpdateAccessTokenRepository } from '../../protocols/db/update-access-token-repository'
 
 export class DbAuthentication implements Authentication {
     private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository
     private readonly hashCompare: HashCompare
     private readonly tokenGenerator: TokenGenerator
+    private readonly updateAccessTokenRepository: UpdateAccessTokenRepository
 
     constructor(
         loadAccountByEmailRepository: LoadAccountByEmailRepository,
         hashCompare: HashCompare,
-        tokenGenerator: TokenGenerator
+        tokenGenerator: TokenGenerator,
+        updateAccessTokenRepository: UpdateAccessTokenRepository
     ) {
         this.loadAccountByEmailRepository = loadAccountByEmailRepository
         this.hashCompare = hashCompare
         this.tokenGenerator = tokenGenerator
+        this.updateAccessTokenRepository = updateAccessTokenRepository
     }
 
     async auth(authentication: AuthenticationModel): Promise<string> {
@@ -33,6 +37,10 @@ export class DbAuthentication implements Authentication {
             if (accountPassword) {
                 const accessToken = await this.tokenGenerator.generate(
                     account.id
+                )
+                await this.updateAccessTokenRepository.update(
+                    account.id,
+                    accessToken
                 )
                 return accessToken
             }
