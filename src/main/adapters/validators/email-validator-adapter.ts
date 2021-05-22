6bf29374e4895019d66eb3f56ea26e4e186284ec
Victor Shermon <victorshermon@yahoo.com>
2021-05-21 22:58:10 -0300

    refactor: move EmailValidatorAdapter to the main layer

--- src/main/adapters/validators/email-validator-adapter.ts
+++ src/main/adapters/validators/email-validator-adapter.ts
@@ -1,4 +1,4 @@
-import { EmailValidator } from '../presentation/helpers/email-validator'
+import { EmailValidator } from '../../../presentation/helpers/email-validator'
 import validator from 'validator'
 
 export class EmailValidatorAdapter implements EmailValidator {
