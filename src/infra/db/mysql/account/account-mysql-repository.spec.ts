b1fd20866c25f75342102844cad98158c750e1c8
Victor Shermon <victorshermon@yahoo.com>
2021-05-26 17:32:21 -0300

    test: ensure AccountMysqlRepositorywill not find an account if find account by token fails

--- src/infra/db/mysql/account/account-mysql-repository.spec.ts
+++ src/infra/db/mysql/account/account-mysql-repository.spec.ts
@@ -109,4 +109,9 @@ describe('Account Mongo Repository', () => {
         expect(account.email).toBe('any_email@mail.com')
         expect(account.password).toBe('any_password')
     })
+    test('Should return null if loadByToken  fails', async () => {
+        const sut = makeSut()
+        const account = await sut.load('any_email@mail.com')
+        expect(account).toBeFalsy()
+    })
 })
