d8ca573f71ba54f62ce2619e0d96432e53f29645
Victor Shermon <victorshermon@yahoo.com>
2021-05-26 17:19:11 -0300

    test: ensure  AccountMysqlRepository find an account by token with role

--- src/infra/db/mysql/account/account-mysql-repository.spec.ts
+++ src/infra/db/mysql/account/account-mysql-repository.spec.ts
@@ -91,4 +91,22 @@ describe('Account Mongo Repository', () => {
         expect(account.email).toBe('any_email@mail.com')
         expect(account.password).toBe('any_password')
     })
+    test('Should return an account on loadByToken with role', async () => {
+        const sut = makeSut()
+        const signup = accountRepository.create({
+            name: 'any_name',
+            email: 'any_email@mail.com',
+            sexo: 'M',
+            password: 'any_password',
+            token: 'any_token',
+            role: 'any_role'
+        })
+        await accountRepository.save(signup)
+        const account = await sut.load('any_token', 'any_role')
+        expect(account).toBeTruthy()
+        expect(account.id).toBeTruthy()
+        expect(account.name).toBe('any_name')
+        expect(account.email).toBe('any_email@mail.com')
+        expect(account.password).toBe('any_password')
+    })
 })
