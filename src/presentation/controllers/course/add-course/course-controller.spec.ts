a47bc2f37654d13c7d4bc7af59114efbefd99e83
Victor Shermon <victorshermon@yahoo.com>
2021-05-27 21:29:57 -0300

    test: ensure should call AddCourseController how correct values

--- src/presentation/controllers/course/add-course/course-controller.spec.ts
+++ src/presentation/controllers/course/add-course/course-controller.spec.ts
@@ -1,3 +1,8 @@
+import { CourseModel } from '../../../../domain/models/course-model'
+import {
+    AddCourse,
+    AddCourseModel
+} from '../../../../domain/usecases/add-course/add-course'
 import { MissingParamError } from '../../../erros'
 import { badRequest } from '../../../helpers/http/http-helper'
 import { Validation } from '../../signup/signup-controller-protocols'
@@ -6,6 +11,7 @@ import { AddCourseController } from './course-controller'
 interface SutTypes {
     sut: AddCourseController
     validationStub: Validation
+    addCourseStub: AddCourse
 }
 
 const makeValidation = (): Validation => {
@@ -17,12 +23,30 @@ const makeValidation = (): Validation => {
     return new ValidationStub()
 }
 
+const makeFakeResponseCourse = (): CourseModel => ({
+    id: 'any_id',
+    title: 'any_title',
+    figure: 'any_figure',
+    slug: 'any_slug'
+})
+
+const makeAddCourse = (): AddCourse => {
+    class AddCourseStub implements AddCourse {
+        async add(course: AddCourseModel): Promise<CourseModel> {
+            return new Promise((resolved) => resolved(makeFakeResponseCourse()))
+        }
+    }
+    return new AddCourseStub()
+}
+
 const makeSut = (): SutTypes => {
     const validationStub = makeValidation()
-    const sut = new AddCourseController(validationStub)
+    const addCourseStub = makeAddCourse()
+    const sut = new AddCourseController(validationStub, addCourseStub)
     return {
         sut,
-        validationStub
+        validationStub,
+        addCourseStub
     }
 }
 
@@ -52,5 +76,20 @@ describe('Course Controller', () => {
         )
         const courseError = await sut.handle(httpRequest)
         expect(courseError).toEqual(badRequest(new MissingParamError('figure')))
+    })
+    test('should call AddCourseController how correct values', async () => {
+        const { sut, addCourseStub } = makeSut()
+        const httpRequest = {
+            body: {
+                title: 'any_title',
+                figure: 'any_figure'
+            }
+        }
+        const spyAddCourse = jest.spyOn(addCourseStub, 'add')
+        await sut.handle(httpRequest)
+        expect(spyAddCourse).toHaveBeenCalledWith({
+            title: 'any_title',
+            figure: 'any_figure'
+        })
     })
 })
