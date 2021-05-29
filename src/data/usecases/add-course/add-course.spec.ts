a632529dfd3582351016cd8bdfcc0469bc4886b8
Victor Shermon <victorshermon@yahoo.com>
2021-05-29 09:15:53 -0300

    refactor: leaving the code cleaner

--- src/data/usecases/add-course/add-course.spec.ts
+++ src/data/usecases/add-course/add-course.spec.ts
@@ -1,21 +1,40 @@
+import { AddCourse } from '../../../domain/usecases/add-course/add-course'
 import { Slug } from '../../protocols/remodulate/slug'
 import { DbAddCourse } from './add-course'
 
+interface SutTypes {
+    sut: AddCourse
+    slugStub: Slug
+}
+
+const makeFakeRequest = () => ({
+    title: 'any_title',
+    figure: 'any_value'
+})
+
+const makeSlugStub = (): Slug => {
+    class SlugStub implements Slug {
+        transform(value: string): string {
+            return 'any-value'
+        }
+    }
+    return new SlugStub()
+}
+
+const makeSut = (): SutTypes => {
+    const slugStub = makeSlugStub()
+    const sut = new DbAddCourse(slugStub)
+    return {
+        sut,
+        slugStub
+    }
+}
+
 describe('DbAddCourse Usecase', () => {
     test('Should call Slug with correct value', async () => {
-        class SlugStub implements Slug {
-            transform(value: string): string {
-                return 'any-value'
-            }
-        }
-        const makeFakeRequest = {
-            title: 'any_title',
-            figure: 'any_value'
-        }
-        const slugStub = new SlugStub()
-        const sut = new DbAddCourse(slugStub)
+        const { sut, slugStub } = makeSut()
         const spySlug = jest.spyOn(slugStub, 'transform')
-        await sut.add(makeFakeRequest)
+        await sut.add(makeFakeRequest())
         expect(spySlug).toHaveBeenCalledWith('any_title')
     })
 })
