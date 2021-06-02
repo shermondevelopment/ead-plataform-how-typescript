bac2b9c4f87f79e5a0a606e3f10819d441edd9bc
Victor Shermon <victorshermon@yahoo.com>
2021-06-02 20:51:02 -0300

    feat: Should return 500 if loadCourse throws

--- src/presentation/controllers/course/show-course/course-controller.ts
+++ src/presentation/controllers/course/show-course/course-controller.ts
@@ -1,12 +1,16 @@
 import { LoadCourses } from '../../../../domain/usecases/load-courses/load-course'
+import { serverError } from '../../../helpers/http/http-helper'
 import { HttpRequest, HttpResponse, Controller } from '../../../protocols'
 
 export class LoadCourseController implements Controller {
     constructor(private readonly courses: LoadCourses) {}
 
     async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-        const { search, page } = httpRequest.query
-        this.courses.load({ search, page })
-        return null
+        try {
+            const { search, page } = httpRequest.query
+            await this.courses.load({ search, page })
+        } catch (error) {
+            return serverError(error)
+        }
     }
 }
