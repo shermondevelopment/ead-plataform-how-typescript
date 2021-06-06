ec402576696320e06b97bbe727f05651f3c6dbb2
Victor Shermon <victorshermon@yahoo.com>
2021-06-06 15:57:04 -0300

    feat: ensure should call DbDeleteRepository how correct values

---
+++ src/data/usecases/delete-course/db-delete-course.spec.ts
@@ -0,0 +1,40 @@
+import {
+    Delete,
+    ReturnDelete,
+    DeleteParam
+} from '../../../domain/usecases/delete-course/delete'
+import { DbDeleteCourse } from './db-delete-course'
+
+interface SutTypes {
+    sut: DbDeleteCourse
+    dbDeleteRepositoryStub: Delete
+}
+
+const makeDbDeleteRepositoryStub = (): Delete => {
+    class DbDeleteRepository implements Delete {
+        async delete(param: DeleteParam): Promise<ReturnDelete> {
+            return new Promise((resolved) => resolved({ delete: true }))
+        }
+    }
+    return new DbDeleteRepository()
+}
+
+const makeSut = (): SutTypes => {
+    const dbDeleteRepositoryStub = makeDbDeleteRepositoryStub()
+    const sut = new DbDeleteCourse(dbDeleteRepositoryStub)
+    return {
+        sut,
+        dbDeleteRepositoryStub
+    }
+}
+
+describe('DbDeleteCourse', () => {
+    test('Should call DbDeleteRepository how correct values', async () => {
+        const { sut, dbDeleteRepositoryStub } = makeSut()
+        const spyDelete = jest.spyOn(dbDeleteRepositoryStub, 'delete')
+        await sut.delete({ id: 'any_id' })
+        expect(spyDelete).toHaveBeenCalledWith({
+            id: 'any_id'
+        })
+    })
+})
