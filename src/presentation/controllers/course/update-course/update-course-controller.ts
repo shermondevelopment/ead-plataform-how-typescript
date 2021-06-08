0599c9136c0a56fa105be4ace7c3bbc7a5d2ec14
Victor Shermon <victorshermon@yahoo.com>
2021-06-08 20:58:36 -0300

    feat: ensure should return 500 if UpdateCourse return throws

--- src/presentation/controllers/course/update-course/update-course-controller.ts
+++ src/presentation/controllers/course/update-course/update-course-controller.ts
@@ -1,11 +1,16 @@
 import { UpdateCourse } from '../../../../domain/usecases/update-course/update-course'
+import { serverError } from '../../../helpers/http/http-helper'
 import { Controller, HttpRequest, HttpResponse } from '../../../protocols'
 
 export class UpdateCourseController implements Controller {
     constructor(private readonly updateCourse: UpdateCourse) {}
 
     async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-        this.updateCourse.update(httpRequest.body)
-        return null
+        try {
+            await this.updateCourse.update(httpRequest.body)
+            return null
+        } catch (error) {
+            return serverError(error)
+        }
     }
 }
