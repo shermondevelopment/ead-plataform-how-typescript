0713e91938ea08ca235e9cf7c7c36e6be6b5cfd0
Victor Shermon <victorshermon@yahoo.com>
2021-06-09 21:03:52 -0300

    feat: ensure should call UpdateCourseRepository how correct values

--- src/data/usecases/course/update-course/db-update-course.ts
+++ src/data/usecases/course/update-course/db-update-course.ts
@@ -1,16 +1,25 @@
 import { UpdateCourse } from '../../../../domain/usecases/update-course/update-course'
+import { UpdateCourseRepository } from '../../../protocols/db/course/db-update-course-repository'
 import { Slug } from '../../../protocols/remodulate/slug'
 import { CourseModel } from '../loading-course/loading-course-protocols'
 
 export class DbUpdateCourse implements UpdateCourse {
-    constructor(private readonly slug: Slug) {}
+    constructor(
+        private readonly slug: Slug,
+        private readonly updateCourseRepository: UpdateCourseRepository
+    ) {}
 
     async update(
         courseModel: Partial<CourseModel>
     ): Promise<Partial<CourseModel>> {
         if (courseModel.title) {
-            this.slug.transform(courseModel.title)
+            const update = await this.updateCourseRepository.update({
+                ...courseModel,
+                slug: this.slug.transform(courseModel.title)
+            })
+            return update
         }
+        await this.updateCourseRepository.update({ ...courseModel })
         return null
     }
 }
