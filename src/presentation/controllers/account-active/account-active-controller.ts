cef44f94083e6f706b489faf8cd8fb7e790af024
Victor Shermon <victorshermon@yahoo.com>
2021-06-14 21:11:45 -0300

    feat: ensure should return throw if ActiveAccount return throw

--- src/presentation/controllers/account-active/account-active-controller.ts
+++ src/presentation/controllers/account-active/account-active-controller.ts
@@ -1,17 +1,21 @@
 import { ActiveAccount } from '../../../domain/usecases/active-account/active-account'
 import { InvalidParamError } from '../../erros'
-import { badRequest } from '../../helpers/http/http-helper'
+import { badRequest, serverError } from '../../helpers/http/http-helper'
 import { Controller, HttpRequest, HttpResponse } from '../../protocols'
 
 export class ActiveAccountController implements Controller {
     constructor(private readonly accountActive: ActiveAccount) {}
 
     async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-        const { token } = httpRequest.query
-        const active = await this.accountActive.accountActive({ token })
-        if (!active) {
-            return badRequest(new InvalidParamError('token'))
+        try {
+            const { token } = httpRequest.query
+            const active = await this.accountActive.accountActive({ token })
+            if (!active) {
+                return badRequest(new InvalidParamError('token'))
+            }
+            return null
+        } catch (error) {
+            return serverError(error)
         }
-        return null
     }
 }
